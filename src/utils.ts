455e5efe7e7718163c09646771e3f2fc7496a756
princewill owoh <princewillowoh18@gmail.com>
2025-11-26 09:56:04 +0100

    changes

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,14 @@ const yearsOfExperience = calculateExperience(2020);
 export const HEAD_CONTENT = `Hey there! I'm a Frontend Developer with ${yearsOfExperience} years of hands-on experience, passionate about creating beautiful and functional interfaces across various frontend technologies like React and Next.js. I specialize in developing scalable and maintainable frontends that offer exceptional user experiences and robust functionality`;
 export const PROJECTS = [
   {
+    title: "Showfindr",
+    url: "https://showfindr.co/",
+    image: "https://i.imgur.com/aRqvzR4.png",
+    description:
+      "A full-stack application providing users with a seamless, mobile-first experience for discovering, comparing, and instantly booking tickets for live events (music, sports, theater). The project emphasized performance and intuitive user flows across the entire platform.",
+    technologies: ["tailwind", "Nextjs", "Expressjs"],
+  },
+  {
     title: "Demicare",
     url: "https://www.demicare.skin/",
     image: "https://i.imgur.com/326JnBE.jpeg",
@@ -20,22 +28,22 @@ export const PROJECTS = [
   },
   {
     title: "Tripplug",
-    url:"https://www.tripplug.com/",
+    url: "https://www.tripplug.com/",
     image: "https://i.imgur.com/KJJbhhD.jpeg",
 
     description:
       "This web application serves as a comprehensive online platform enabling users to search, compare, book, and manage various travel services (flights, hotels, vacation packages, car rentals, visa assistance, etc.) from multiple providers, offering a seamless and secure booking experience.",
     technologies: ["HTML", "CSS", "Tailwind", "Nextjs", "Amadeus API"],
   },
-  {
-    title: "Celeb Beauty Clinics",
-    url: "https://celebbeautyclinics.com/",
-    image: "https://i.imgur.com/O0O7uGg.jpeg",
+  // {
+  //   title: "Celeb Beauty Clinics",
+  //   url: "https://celebbeautyclinics.com/",
+  //   image: "https://i.imgur.com/O0O7uGg.jpeg",
 
-    description:
-      "A  medical spa offering advanced aesthetic treatments. The primary goal was to create a digital presence that effectively communicates the spa's blend of luxurious relaxation and medical-grade services, attracting new clients while providing existing ones with easy access to information",
-    technologies: ["HTML", "CSS", "Reactjs", "Tailwind"],
-  },
+  //   description:
+  //     "A  medical spa offering advanced aesthetic treatments. The primary goal was to create a digital presence that effectively communicates the spa's blend of luxurious relaxation and medical-grade services, attracting new clients while providing existing ones with easy access to information",
+  //   technologies: ["HTML", "CSS", "Reactjs", "Tailwind"],
+  // },
 ];
 
 export const EXPERIENCES = [
@@ -91,7 +99,7 @@ export const CONTACT = {
   email: "princewillowoh18@gmail.com",
 };
 
-function calculateExperience(startYear) {
+function calculateExperience(startYear: number) {
   const currentYear = new Date().getFullYear(); // Gets the current year
   return currentYear - startYear;
 }
